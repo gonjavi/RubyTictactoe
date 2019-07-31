eac8dc97b37ed592c6fb8104c4f5413ea57900f4
gonjavi <javiercaliescali@gmail.com>
2019-07-31 16:30:06 -0500

    show board

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,9 @@
 class Game
+    attr_accessor :board
 
     def initialize
+        #to intialize start method and winning positions
+        start
         @WINNING_POSITIONS =
       [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     end
@@ -10,7 +13,7 @@ class Game
          @player = "x"
         @board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
     end
-    def show_board(board=@board)
+    def show_board(board = @board)
         puts
         puts board[0..2].join(" ")
         puts board[3..5].join(" ")
@@ -21,13 +24,27 @@ class Game
     def play
        
 
-        play_game = true
-        while play_game
+        @play_game = true
+        while @play_game
             show_board
+            puts "press the letter (q) if you want to exit at any time"
+            end_game
 
         end
+        
     end 
+    def end_game
+        x=gets.chomp
+        if x== "q"
+            print "Do you want to finish the game? y/n"
+            finish=gets.chomp
+            if finish=="y"
+                @play_game=false
+            end
+        end
+      end
 
+   
 end
 
 board = Game.new
